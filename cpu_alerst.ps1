8c3ac4cffb9cf3d9555fd4082344a4800447baae
kissk86 <64041899+kissk86@users.noreply.github.com>
2020-09-16 12:07:26 +0200

    Update cpu_alerst.ps1

--- cpu_alerst.ps1
+++ cpu_alerst.ps1
@@ -1,5 +1,5 @@
 # Telegram CPU alert - Powershell script
-# version: 2020-09-13
+# version: 2020-09-16
 # Send Telegram message | Telegram: https://telegram.org/
 # Users can interact with bots by sending them messages  | Telegram bot: https://core.telegram.org/bots
 # PoshGram provides functionality to send various message types to a specified Telegram chat via the Telegram Bot API | PoshGram: https://github.com/techthoughts2/PoshGram
@@ -23,17 +23,17 @@ $waiting_time = 1800 # 30 min
 $CpuLoad_previous_value = 0
 $chat_msg = "CPU is under load"
 
-while (1 -eq 1 ) {
+while ($true) {
     $CpuLoad = powershell "(Get-WmiObject win32_processor | Measure-Object -property LoadPercentage -Average | Select Average ).Average"
     
-    Write-Host $(get-date -f yyyy-MM-dd) - $(Get-Date -f HH-mm)
+    Write-Host $(get-date -f yyyy-MM-dd) - $(Get-Date -f HH:mm)
     Write-Host "CPU previous value:    $CpuLoad_previous_value"
     Write-Host "CPU value: $CpuLoad"
     Write-Host "* * * * * * * * * * *"
 
     if (($CpuLoad -gt $cpu_alert_value) -AND ($CpuLoad_previous_value -gt $cpu_alert_value)) {
         # Send message
-        Write-Host "Send message - "$(get-date -f yyyy-MM-dd) - $(Get-Date -f HH-mm)
+        Write-Host "Send message"$(get-date -f yyyy-MM-dd) - $(Get-Date -f HH:mm)
         Send-TelegramTextMessage -bottoken $MyToken -ChatID $ChatID -Message $chat_msg
     }
 
